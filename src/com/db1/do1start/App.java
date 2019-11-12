f0bca6c4ed0908dab0958da47844f9f6651688b3
leonardo <leonardonasser11@hotmail.com>
2019-11-12 15:55:23 -0200

    Ex3 Double - Menor C/função retonando em double

--- src/com/db1/do1start/App.java
+++ src/com/db1/do1start/App.java
@@ -6,22 +6,27 @@ import java.util.Scanner;
 public class App {
 
     //Usando função
-    public static String menornumero(Double valor1, Double valor2){
-    String resultado;
+    public static Double menornumero(Double valor1, Double valor2, Double valor3){
+    Double resultado;
 
-        if (valor1 < valor2) {
-            resultado="Menor valor é o 1";
-        } else {
-            resultado="Menor valor é o 2";
+        if (valor1<valor2 && valor1<valor3) {
+            resultado=valor1;
         }
-
-    return resultado;
+//=============
+        if (valor2<valor1 && valor2<valor3){
+            resultado=valor2;
+        }
+//==============
+        if (valor3<valor1 && valor3<valor2){
+            resultado=valor3;
+        }
+        return resultado;
     }
 
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
-        Double numero1,numero2;
-        String result;
+        Double numero1,numero2,numero3;
+        Double result;
 
         System.out.println("Digite o numero1: ");
         numero1 = ler.nextDouble();
@@ -29,9 +34,12 @@ public class App {
         System.out.println("Digite o numero2: ");
         numero2 = ler.nextDouble();
 
+        System.out.println("Digite o numero3: ");
+        numero3 = ler.nextDouble();
+
         //retornando a função
-        result = menornumero(numero1,numero2);
-        System.out.println(result);
+        result = menornumero(numero1,numero2,numero3);
+        System.out.println("O valor menor é o: "+result);
 
 
      }
