f7ac3af48e4ef1a30efe1f69555de6a2862f155d
leonardo <leonardonasser11@hotmail.com>
2019-11-12 17:10:41 -0200

    ARTE DE FAZER TESTES 2

--- src/com/db1/do1start/Application.java
+++ src/com/db1/do1start/Application.java
@@ -3,6 +3,7 @@ package com.db1.do1start;
 public class Application {
     public Integer soma(Integer numero1,Integer numero2){
        Integer resultado;
+
        numero1=10;
        numero2=10;
 
--- src/com/db1/do1start/Apptest.java
+++ src/com/db1/do1start/Apptest.java
@@ -6,12 +6,19 @@ package com.db1.do1start;
 public class Apptest {
 
     @Test
-    public void teste() {
+    public void deveser19() {
         Application application = new Application();
         Integer expected = 19;
         Integer response = application.soma(2, 3);
         Assert.assertEquals(expected, response);
+    }
 
+        @Test
+        public void Naodeveser10() {
+            Application application = new Application();
+            Integer expected = 10;
+            Integer response = application.soma(2, 3);
+            Assert.assertNotEquals(expected, response);
 
 
     }
