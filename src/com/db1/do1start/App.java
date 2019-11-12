d34df0878ea5b1ac10ac1dd5658eb6630b6a96c6
leonardo <leonardonasser11@hotmail.com>
2019-11-12 15:29:16 -0200

    Ex1 TEXTO C/função

--- src/com/db1/do1start/App.java
+++ src/com/db1/do1start/App.java
@@ -6,27 +6,26 @@ import java.util.Scanner;
 public class App {
 
     //Usando função
-    public static Integer soma(Integer num1,Integer num2){
-    Integer resultado;
+    public static String maiusculo(String textoM){
+    String resultado;
 
-    resultado= num1-num2;
+
+    resultado= textoM.toUpperCase();
 
     return resultado;
     }
 
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
-        int result, num1, num2;
+        String texto,result;
 
-        System.out.println("Digite o num1: ");
-        num1 = ler.nextInt();
+        System.out.println("Digite o texto: ");
+        texto = ler.next();
 
-        System.out.println("Digite o num2: ");
-        num2 = ler.nextInt();
 
         //retornando a função
-        result = soma(num1, num2);
-        System.out.println(result);
+        result = maiusculo(texto);
+        System.out.println("Texto em maiusculo: "+result);
 
 
      }
