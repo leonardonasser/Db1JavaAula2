05ec62f552e27f4b82b66ffa29496cf6c4d3a20a
leonardo <leonardonasser11@hotmail.com>
2019-11-12 15:43:52 -0200

    Ex1 Double - Menor C/função

--- src/com/db1/do1start/App.java
+++ src/com/db1/do1start/App.java
@@ -6,26 +6,32 @@ import java.util.Scanner;
 public class App {
 
     //Usando função
-    public static String minusculo(String textoMinus){
+    public static String menornumero(Double valor1, Double valor2){
     String resultado;
 
-
-    resultado= textoMinus.toLowerCase();
+        if (valor1 < valor2) {
+            resultado="Menor valor é o 1";
+        } else {
+            resultado="Menor valor é o 2";
+        }
 
     return resultado;
     }
 
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
-        String texto,result;
+        Double numero1,numero2;
+        String result;
 
-        System.out.println("Digite o texto em maiusculo: ");
-        texto = ler.next();
+        System.out.println("Digite o numero1: ");
+        numero1 = ler.nextDouble();
 
+        System.out.println("Digite o numero2: ");
+        numero2 = ler.nextDouble();
 
         //retornando a função
-        result = minusculo(texto);
-        System.out.println("Texto em minusculo: "+result);
+        result = menornumero(numero1,numero2);
+        System.out.println(result);
 
 
      }
