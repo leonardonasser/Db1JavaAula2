908ece8051d7b2efc12a9eabda64e82b331824f9
leonardo <leonardonasser11@hotmail.com>
2019-11-13 17:25:57 -0200

    tudo

--- src/com/db1/do1start/Application.java
+++ src/com/db1/do1start/Application.java
@@ -1,18 +1,132 @@
 package com.db1.do1start;
 
-
-
+//==============================Exercícios de Inteiro (Integer)===================================================
+//7 - Método que a partir de um valor inicial, mostre a quantidade de n ímpares até ele.
 public class Application {
-    public Integer soma(Integer numero){
+        public Integer InteiroEx7Desafio(Integer numero){
 
-        numero=50;
-        Integer x=0;
+            numero=50;
+            numero=numero/2;
+
+            return numero;
 
 
-        return numero;
+        }
 
+//=================================Exercícios de Texto (String)====================================================
+
+
+ //6 - Método que receba o seu nome completo e exiba somente as 4 primeiras letras do seu nome
+    public String Ex6destring(String meunome){
+             Integer x;
+             String Primeirasletras= "";
+             meunome= "Leonardo Mercado Nasser";
+        Integer Quantidadedeletras=(meunome.length());
+
+        for(x=0;x<4;x++){
+            Primeirasletras += meunome.charAt(x);
+        }
+            return Primeirasletras;
+
+        }
+
+ //7 - Método que receba o seu nome completo e exiba a partir da terceira letra do seu nome
+ public String Ex7destring(String meunome){
+     Integer x;
+     String letras= "";
+     meunome= "Leonardo Mercado Nasser";
+     Integer Quantidadedeletras=(meunome.length());
+
+     for(x=2;x<Quantidadedeletras;x++){
+        letras += meunome.charAt(x);
+     }
+     return letras;
+
+ }
+
+ //8 - Método que receba o seu nome completo e exiba somente as 4 ultimas letras do seu nome
+    public String Ex8destring(String meunome){
+        Integer x;
+        String letras= "";
+        meunome= "Leonardo Mercado Nasser";
+        Integer Quantidadedeletras=(meunome.length());
+
+        for(x=18;x<Quantidadedeletras;x++){
+            letras += meunome.charAt(x);
+        }
+        return letras;
+        }
+
+//9 - Método que receba o seu nome completo e substitua o seu primeiro nome por ALUNO/ALUNA
+    public String Ex9destring(String meunome){
+        Integer x;
+        String letras= "Aluno";
+        meunome= "Leonardo Mercado Nasser";
+        Integer Quantidadedeletras=(meunome.length());
+
+        for(x=8;x<Quantidadedeletras;x++){
+            letras += meunome.charAt(x);
+        }
+        return letras;
+    }
+
+  /*  //10 - Método que receba o seguinte texto "banana, maçã, melancia" e exiba o texto separadamente.
+    public String[] Ex10destring(String texto){
+        texto = "banana, maçã, melancia";
+        String separado[] = texto.split(',');
+
+        return separado;
+    }*/
+
+  //11 - Método que receba um texto e exiba quantas vogais tem no texto
+    public Integer Ex11destring(Integer cont){
+            String vogaisMaiusculo="AEIOU";
+            String vogaisMinusculo="aeiou";
+            Integer x,y;
+            String texto="Db1start";
+            Integer Quantidadedeletras=(texto.length());
+
+        for(x=8;x<Quantidadedeletras;x++){
+           for(y=0;y<5;y++) {
+               if (vogaisMaiusculo.charAt(y) == texto.charAt(x) || vogaisMinusculo.charAt(y) == texto.charAt(x)) {
+                   cont = cont + 1;
+               }
+           }
+        }
+        return cont;
+    }
+
+    //12 - Método que receba um texto e devolva ele invertido
+    public String Ex12destring(String texto){
+        texto = "texto invertido";
+        String inverter ="";
+        Integer Quantidadedeletras=texto.length(),x;
+
+        for(x=Quantidadedeletras;x<0;x++){
+            inverter += texto.charAt(x);
+        }
+        return inverter;
+    }
+
+
+
+
+//===================================Exercícios de Matemática (Double)========================================
+
+//1 - Método que exiba qual é o menor valor entre dois doubles
+    public Boolean Ex1deMatematica(Double valor1,Double valor2){
+       valor1=5;
+       valor2=10;
+
+        if (valor1 < valor2) {
+           return true;
+        } else {
+           return false;
+        }
 
     }
+
+
 
 
 }
--- src/com/db1/do1start/Apptest.java
+++ src/com/db1/do1start/Apptest.java
@@ -5,32 +5,109 @@ import org.junit.Test;
 
 public class Apptest {
 
+
+
+    //  Exercícios de Inteiro (Integer)
+
+//7 - Método que a partir de um valor inicial, mostre a quantidade de n ímpares até ele. -MODO DESAFIO
     @Test
-    public void ParaParLer1() {
+    public void  InteiroEx7Desafio() {
         Application application = new Application();
-        Integer expected = 50;
-        Integer response = application.soma(30);
+        Integer expected = 25;
+        Integer response = application.InteiroEx7Desafio(30);
+        Assert.assertEquals(expected, response);
+        System.out.println("\n No total tem: "+expected+" impares");
+
+
+    }
+
+    //Exercícios de Texto (String)
+
+
+    //6 - Método que receba o seu nome completo e exiba a partir da terceira letra do seu nome
+    @Test
+    public void  Ex6destring() {
+        Application application = new Application();
+        String response = application.Ex6destring("leonardo");
+        String expected = "Leon";
         Assert.assertEquals(expected, response);
 
-        Integer cont=0;
-
-        cont=expected/2;
-
-        System.out.println("\n No total tem: "+cont+" impares");
-
-
     }
 
-/*
+    //7 - Método que receba o seu nome completo e exiba a partir da terceira letra do seu nome
     @Test
-    public void Paraimparler() {
+    public void  Ex7destring() {
         Application application = new Application();
-        boolean expected = false;
-        boolean response = application.soma(1);
-        Assert.assertNotEquals(expected, response);
-        if (expected == false) {
-            System.out.println("NUMERO IMPAR");
-        }
+        String response = application.Ex7destring("leonardo");
+        String expected = "onardo Mercado Nasser";
+        Assert.assertEquals(expected, response);
+
     }
-*/
+    //8 - Método que receba o seu nome completo e exiba somente as 4 ultimas letras do seu nome
+
+    @Test
+    public void  Ex8destring() {
+        Application application = new Application();
+        String response = application.Ex8destring("leonardo");
+        String expected = "asser";
+        Assert.assertEquals(expected, response);
+
+    }
+
+    //9 - Método que receba o seu nome completo e substitua o seu primeiro nome por ALUNO/ALUNA
+
+    @Test
+    public void  Ex9destring() {
+        Application application = new Application();
+        String response = application.Ex9destring("leonardo");
+        String expected = "Aluno Mercado Nasser";
+        Assert.assertEquals(expected, response);
+    }
+
+    //10 - Método que receba o seguinte texto "banana, maçã, melancia" e exiba o texto separadamente.
+  /*  @Test
+    public void  Ex10destring() {
+        Application application = new Application();
+        String expected[]={"banana", "maçã", "melancia"};
+        String response = application.Ex10destring("");
+        Assert.assertEquals(expected, response);
+    }*/
+
+   // 11 - Método que receba um texto e exiba quantas vogais tem no texto
+   @Test
+   public void  Ex11destring() {
+       Application application = new Application();
+       Integer expected = 1;
+       Integer response = application.Ex11destring(1);
+       Assert.assertEquals(expected, response);
+   }
+    //12 - Método que receba um texto e devolva ele invertido
+
+    @Test
+    public void  Ex12destring() {
+        Application application = new Application();
+        String expected = "oditevni otxet";
+        String response = application.Ex12destring("texto invertido");
+        Assert.assertEquals(expected, response);
+
+        System.out.println("Texto invertido: "+expected);
+    }
+
+//===================================Exercícios de Matemática (Double)========================================
+
+//1 - Método que exiba qual é o menor valor entre dois doubles
+    @Test
+public void  Ex1deMatematica() {
+    Application application = new Application();
+    Boolean expected = true;
+    Boolean response = application.Ex1deMatematica(10,5);
+    Assert.assertEquals(expected, response);
+
+    if(expected==true){
+        System.out.println("1 menor");
+    }else{
+        System.out.println("2 menor");
+    }
+   }
+
 }
